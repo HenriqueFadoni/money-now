6fb881238fdcbc0c70680b7f0d83dd08bbee7111
Henrique Fadoni <henriquefadonii@gmail.com>
2019-03-03 16:31:58 -0300

    Currency Converter

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ class App extends Component {
   state = {
     base: '',
     toCurrency: '',
+    baseValue: 0,
+    currencyValue: 0,
     date: '',
     rates: {}
   }
 
   async componentDidMount() {
-    const {data} = await axios.get(`https://api.exchangeratesapi.io/latest?base=${this.state.base}`);
+    const { data } = await axios.get(`https://api.exchangeratesapi.io/latest?base=${this.state.base}`);
+    this.setState({ rates: data.rates });
+  };
+
+  selectBaseHandler = event => this.getData(event.target.value);
+
+  getData = async (currency) => {
+    const { data } = await axios.get(`https://api.exchangeratesapi.io/latest?base=${currency}`);
     this.setState({
       base: data.base,
       date: data.date,
@@ -18,30 +27,48 @@ class App extends Component {
     });
   }
 
-  selectBaseHandler = event => this.setState({base: event.target.value});
-  selectToHandler = event => this.setState({toCurrency: event.target.value});
+  selectToHandler = event => this.setState({ toCurrency: event.target.value });
+
+  findRate = () => {
+    const value = Math.round(this.state.rates[this.state.toCurrency] * 100) / 100;
+    this.setState({ 
+      baseValue: 1,
+      currencyValue: value
+    });
+  }
 
   render() {
     const rates = this.state.rates;
     let arrayRates = [];
+
     for (let rate in rates) {
       arrayRates.push(
-      <option value={`${rate}`} key={`${rate}`} >
-        {rate}
-      </option>
+        <option value={`${rate}`} key={`${rate}`} >
+          {rate}
+        </option>
       );
     }
+
     return (
       <div className="App">
-        <select id="base-currency-selector" onChange={event => this.selectBaseHandler(event)}>
+        <select id="base-currency-selector" onChange={event => this.selectBaseHandler(event)} required>
           <option value="" defaultValue>Please Select a Base Currency</option>
           {arrayRates}
         </select>
 
-        <select id="to-currency-selector" onChange={event => this.selectToHandler(event)}>
+        <select id="to-currency-selector" onChange={event => this.selectToHandler(event)} required>
           <option value="" defaultValue>Please Select a Currency to Convert</option>
           {arrayRates}
         </select>
+
+        {/* Make it invisible */}
+        <div>
+          <input type="number" placeholder={this.state.baseValue}></input>
+          <input type="number" placeholder={this.state.currencyValue}></input>
+        </div>
+
+        {/* Button Block if Base and Currency not selected */}
+        <button onClick={this.findRate}>Find Currency</button>
       </div>
     );
   }
