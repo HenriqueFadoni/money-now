696b15bcdb87cf88bc9ccdfa4f4fb787768d069a
Henrique Fadoni <henriquefadonii@gmail.com>
2019-03-21 18:00:03 -0300

    Setting Up Redux

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+import rootReducer from './store/reducers/index';
+
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(reduxThunk)
+));
+
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+ReactDOM.render(app, document.getElementById("root"));
 serviceWorker.unregister();
