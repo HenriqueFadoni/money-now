374a128328d4d739bf8b8cfa71d7034c1fce98f3
Henrique Fadoni <henriquefadonii@gmail.com>
2019-03-31 16:09:45 -0300

    Transforming Form component into stateless

--- src/mainPage/Form/Form.js
+++ src/mainPage/Form/Form.js
@@ -1,29 +1,23 @@
 import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
-
 
 import FindRate from './Button/FindRate';
 import BaseInput from './Inputs/Base/BaseInput';
 import CurrencyInput from './Inputs/Currency/CurrencyInput';
 
-class Form extends PureComponent {
-    render() {
-        let inputs = (
-            <>
-                <BaseInput />
-                <CurrencyInput />
-            </>
-        );
+const Form = props => {
+    let inputs = (
+        <>
+            <BaseInput />
+            <CurrencyInput />
+        </>
+    );
 
-        return (
-            <form onSubmit={event => this.props.findRate(event)}>
-                {this.props.showInputs ? inputs : null}
-                <FindRate show={this.props.showBtn} />
-            </form>
-        );
-    }
+    return (
+        <form onSubmit={event => props.findRate(event)}>
+            {props.showInputs ? inputs : null}
+            <FindRate show={props.showBtn} />
+        </form>
+    );
 };
-
-
 
 export default Form;
--- src/mainPage/Form/Inputs/Base/BaseInput.js
+++ src/mainPage/Form/Inputs/Base/BaseInput.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-
 const BaseInput = props => (
     <h2 className="h2-white form__result">
         {props.currencyExchange.baseValue}
