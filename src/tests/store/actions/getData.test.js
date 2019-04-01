b3bd8b29d4665a1cd31003792e85d7acc7ba57f1
Henrique Fadoni <henriquefadonii@gmail.com>
2019-04-01 10:12:10 -0300

    Testing if the Action Creator getData is working properly

---
+++ src/tests/store/actions/getData.test.js
@@ -0,0 +1,57 @@
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import { rootReducer } from '../../../store/reducers/index';
+
+import axios from 'axios';
+
+import { getData, update } from '../../../store/actions/getData';
+
+describe('GetData action creator', () => {
+    let store
+    beforeEach(() => {
+        store = createStore(rootReducer, applyMiddleware(reduxThunk));
+    });
+
+    test('Fetch Data from API Successfuly', async () => {
+        const { data } = await axios.get(`https://api.exchangeratesapi.io/latest`);
+        const dataReceived = {
+            base: 'EUR',
+            date: '',
+            rates: data.rates
+        };
+
+        return store.dispatch(getData())
+            .then(() => {
+                const newState = store.getState();
+                expect(newState.getData.base).toBe('EUR');
+                expect(newState.getData.rates).toEqual(dataReceived.rates);
+            });
+    });
+
+    test('Update State Successfuly', async () => {
+        const initialState = {
+            base: 'EUR',
+            date: '',
+            rates: { 
+                BGN: 1.3038666667,
+                NZD: 1.1,
+                ILS: 2.7176
+            }
+        }
+
+        const { data } = await axios.get(`https://api.exchangeratesapi.io/latest?base=CAD`);
+        const dataReceived = {
+            ...initialState,
+            base: data.base,
+            date: data.date,
+            rates: data.rates
+        };
+
+        return store.dispatch(update('CAD', initialState))
+            .then(() => {
+                const newState = store.getState();
+                console.log(newState);
+                expect(newState.getData).toEqual(dataReceived);
+            });
+    });
+});
