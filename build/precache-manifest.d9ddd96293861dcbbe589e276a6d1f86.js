758f9a8722e6e5ba140374de92b4736a8aa1787b
Henrique Fadoni <henriquefadonii@gmail.com>
2019-04-01 14:02:06 -0300

    Deploying React Application to GitHub pages

---
+++ build/precache-manifest.d9ddd96293861dcbbe589e276a6d1f86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d3d387b654378660352dc352a010dc6",
+    "url": "/money-now/static/media/New-jumbo.3d3d387b.mp4"
+  },
+  {
+    "revision": "0922c3b4be7607de3e53",
+    "url": "/money-now/static/js/runtime~main.b4febeaf.js"
+  },
+  {
+    "revision": "5758169fbd08ec004269",
+    "url": "/money-now/static/js/main.bae1ef45.chunk.js"
+  },
+  {
+    "revision": "9afb89ed2eb412c64279",
+    "url": "/money-now/static/js/2.c8efdbb0.chunk.js"
+  },
+  {
+    "revision": "5758169fbd08ec004269",
+    "url": "/money-now/static/css/main.46b68e4f.chunk.css"
+  },
+  {
+    "revision": "21af7c62877752c2a0e5e4d7cf743a3c",
+    "url": "/money-now/index.html"
+  }
+];
